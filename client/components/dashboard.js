089c3083d5916948ef578b49d6ff7dbecfa61ac7
AlexNikitenko <nikitenko.ae90@gmail.com>
2021-08-06 16:49:24 +0300

    react client task done

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import Head from './head'
-
 const Dashboard = () => {
   return (
     <div>
-      <Head title="Hello" />
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title">Dashboard</div>
-          <Link to="/dashboard/profile/b99901ac-b48e-433e-a0b0-4d28bdd005b4">Go To Profile</Link>
-          <Link to="/dashboard/main">Go To Main</Link>
-        </div>
-      </div>
+      <div id="title">Dashboard</div>
+      <Link to="/dashboard/profile/b99901ac-b48e-433e-a0b0-4d28bdd005b4">Go To Profile</Link>
+      <Link to="/dashboard/main">Go To Main</Link>
     </div>
   )
 }
--- client/components/main.js
+++ client/components/main.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import Head from './head'
-
 const Main = () => {
   return (
     <div>
-      <Head title="Hello" />
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          <div id="title">Main</div>
-          <Link to="/dashboard/profile/b99901ac-b48e-433e-a0b0-4d28bdd005b4">Go To Profile</Link>
-          <Link to="/dashboard">Go To Root</Link>
-        </div>
-      </div>
+      <div id="title">Main</div>
+      <Link to="/dashboard/profile/b99901ac-b48e-433e-a0b0-4d28bdd005b4">Go To Profile</Link>
+      <Link to="/dashboard">Go To Root</Link>
     </div>
   )
 }
