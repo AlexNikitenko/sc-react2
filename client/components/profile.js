fc52fd774fffec6ecf714effe994e38f34f0b6c8
AlexNikitenko <nikitenko.ae90@gmail.com>
2021-08-07 13:57:23 +0300

    react client task done

--- client/components/profile.js
+++ client/components/profile.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Head from './head'
+import Header from './header'
 
 const Profile = () => {
   const { user } = useParams()
   return (
     <div>
       <Head title="Hello" />
+      <Header />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title">Profile</div>
-          <Link to="/dashboard">Go To Root</Link>
-          <Link to="/dashboard/main">Go To Main</Link>
+          <div>
+            <Link to="/dashboard">Go To Root</Link>
+          </div>
+          <div>
+            <Link to="/dashboard/main">Go To Main</Link>
+          </div>
           <div id="username">{user}</div>
         </div>
       </div>
