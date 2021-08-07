bd6d5541b235b5c0baa96002aece2a60dfacb393
AlexNikitenko <nikitenko.ae90@gmail.com>
2021-08-08 00:53:41 +0300

    react client task corrected

--- client/components/dashboard-main.js
+++ client/components/dashboard-main.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Main = () => {
+const DashboardMain = () => {
   return (
     <div>
-      <div>
-        <div id="title">Main</div>
-        <Link to="/dashboard/profile/b99901ac-b48e-433e-a0b0-4d28bdd005b4">Go To Profile</Link>
-      </div>
-      <div>
-        <Link to="/dashboard">Go To Root</Link>
-      </div>
+      <div id="title"> Main </div>
+      <Link to="/dashboard/profile/b99901ac-b48e-433e-a0b0-4d28bdd005b4"> Go To Profile </Link>
+      <Link to="/dashboard"> Go To Root </Link>
     </div>
   )
 }
 
-Main.propTypes = {}
+DashboardMain.propTypes = {}
 
-export default Main
+export default DashboardMain
--- client/components/dashboard-profile.js
+++ client/components/dashboard-profile.js
@@ -5,14 +5,10 @@ const DashboardProfile = () => {
   const { user } = useParams()
   return (
     <div>
-      <div id="title">Profile</div>
-      <div id="username">{user}</div>
-      <div>
-        <Link to="/dashboard">Go To Root</Link>
-      </div>
-      <div>
-        <Link to="/dashboard/main">Go To Main</Link>
-      </div>
+      <div id="title"> Profile </div>
+      <Link to="/dashboard"> Go To Root </Link>
+      <Link to="/dashboard/main"> Go To Main </Link>
+      <div id="username"> {user} </div>
     </div>
   )
 }
