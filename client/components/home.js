9bc5d027ba27fee8007304c7fe98bb0098533cbe
AlexNikitenko <nikitenko.ae90@gmail.com>
2021-08-07 13:29:05 +0300

    react client task done

--- client/components/home.js
+++ client/components/home.js
@@ -5,16 +5,22 @@ import Header from './header'
 import Dashboard from './dashboard'
 import Main from './main'
 import User from './user'
+import Head from './head'
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <Switch>
-        <Route exact path="/dashboard" component={() => <Dashboard />} />
-        <Route exact path="/dashboard/main" component={() => <Main />} />
-        <Route exact path="/dashboard/profile/:user" component={() => <User />} />
-      </Switch>
+      <Head title="Hello" />
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <Switch>
+            <Route exact path="/dashboard" component={() => <Dashboard />} />
+            <Route exact path="/dashboard/main" component={() => <Main />} />
+            <Route exact path="/dashboard/profile/:user" component={() => <User />} />
+          </Switch>
+        </div>
+      </div>
     </div>
   )
 }
