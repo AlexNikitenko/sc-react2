dfb5d1d2159f3e5320f43b39c316361ce732c498
AlexNikitenko <nikitenko.ae90@gmail.com>
2021-08-08 02:21:38 +0300

    react client task corrected

--- client/components/user.js
+++ client/components/user.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useParams, Link } from 'react-router-dom'
 import Head from './head'
+import Header from './header'
 
 const User = () => {
   const { user } = useParams()
   return (
     <div>
+      <Header />
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
