08828bbba56ec42ed4624fdd1672c4bbce47e435
AlexNikitenko <nikitenko.ae90@gmail.com>
2021-08-08 03:32:59 +0300

    refactoring all for react2 test

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import Header from './header'
+import Dashboard from './dashboard'
+import DashboardMain from './dashboard-main'
+import DashboardProfile from './dashboard-profile'
 
 const Home = () => {
   return (
@@ -8,6 +12,11 @@ const Home = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           {/* add your routes here */}
+          <Switch>
+            <Route exact path="/dashboard/" component={() => <Dashboard />} />
+            <Route exact path="/dashboard/main" component={() => <DashboardMain />} />
+            <Route exact path="/dashboard/profile/:user" component={() => <DashboardProfile />} />
+          </Switch>
         </div>
       </div>
     </div>
